1f51b8bad7b338b79e25158c327beeff129250e1
North-West-Wind <wsyn148@gmail.com>
2024-02-08 00:55:39 +0800

    non-working inkfall shirt

--- package.sh
+++ package.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env sh
+
+# clean up
+rm -r build
+mkdir build
+
 zip -j build/splathat.zip \
 	README.md icon.png manifest.json \
 	cosmetics/*.cosmetics
+mkdir -p build/BepInEx/plugins
+cp -r moresuits build/BepInEx/plugins/
+(
+	cd build
+	zip -ur splathat.zip BepInEx -i \*\*/\*.png
+)
